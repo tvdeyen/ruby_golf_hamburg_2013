852f6e4a029f442f340699ce2b638ccbf9be154b
Thomas Jachmann <self@thomasjachmann.com>
2013-11-11 22:37:21 +0100

    Refactor minitest plugin into distinct files

--- minitest/ruby_golf_metrics_plugin.rb
+++ minitest/ruby_golf_metrics_plugin.rb
@@ -1,41 +1,6 @@
-require "sourcify"
+require "minitest/ruby_golf_metrics_reporter"
 
 module Minitest
-
-  class RubyGolfMetricsReporter < Reporter
-    Erg = Struct.new(:method_name, :passed)
-
-    def start
-      @ergs = []
-    end
-
-    def record(erg)
-      @ergs << Erg.new(erg.location.gsub("RubyGolfTest#test_", "").gsub(/ .*/, ""), erg.passed?)
-    end
-
-    def report
-      io.puts "\nRubyGolf metrics (only non whitespace characters are counted)"
-      @ergs.sort_by(&:method_name).each do |erg|
-        if erg.passed
-          begin
-            source = RubyGolf.method(erg.method_name).to_raw_source(strip_enclosure: true)
-            size = source.strip.gsub(" ", "").size
-            io.puts "  #{colorize(erg.method_name, 32)}: #{size} characters"
-          rescue NoMethodError
-            io.puts "  #{colorize(erg.method_name, 31)}: UNDEFINED"
-          end
-        else
-          io.puts "  #{colorize(erg.method_name, 31)}: FAILED"
-        end
-      end
-      io.puts
-    end
-
-    private
-    def colorize(text, color)
-      options[:nocolor] ? text : "\e[#{color}m#{text}\e[0m"
-    end
-  end
 
   def self.plugin_ruby_golf_metrics_options(opts, options)
     opts.on "--nocolor", "Report metrics without color" do
---
+++ minitest/ruby_golf_metrics_reporter.rb
@@ -0,0 +1,43 @@
+require "sourcify"
+
+module Minitest
+
+  class RubyGolfMetricsReporter < Reporter
+
+    Erg = Struct.new(:method_name, :passed)
+
+    def start
+      @ergs = []
+    end
+
+    def record(erg)
+      @ergs << Erg.new(erg.location.gsub("RubyGolfTest#test_", "").gsub(/ .*/, ""), erg.passed?)
+    end
+
+    def report
+      io.puts "RubyGolf metrics (only non whitespace characters are counted)"
+      @ergs.sort_by(&:method_name).each do |erg|
+        if erg.passed
+          begin
+            source = RubyGolf.method(erg.method_name).to_raw_source(strip_enclosure: true)
+            size = source.strip.gsub(" ", "").size
+            io.puts "  #{colorize(erg.method_name, 32)}: #{size} characters"
+          rescue NoMethodError
+            io.puts "  #{colorize(erg.method_name, 31)}: UNDEFINED"
+          end
+        else
+          io.puts "  #{colorize(erg.method_name, 31)}: FAILED"
+        end
+      end
+      io.puts
+    end
+
+    private
+
+    def colorize(text, color)
+      options[:nocolor] ? text : "\e[#{color}m#{text}\e[0m"
+    end
+
+  end
+
+end
