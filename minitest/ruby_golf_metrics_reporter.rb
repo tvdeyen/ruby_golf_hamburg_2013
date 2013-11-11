b2216630a46d83354f4c1d2046eec05745579bd2
Thomas Jachmann <self@thomasjachmann.com>
2013-11-11 23:10:50 +0100

    Also count called methods within RubyGolf

--- minitest/ruby_golf_metrics_reporter.rb
+++ minitest/ruby_golf_metrics_reporter.rb
@@ -1,10 +1,35 @@
 require "sourcify"
+require "minitest/method_parser"
 
 module Minitest
 
   class RubyGolfMetricsReporter < Reporter
 
     Erg = Struct.new(:method_name, :passed)
+
+    class TestMethod
+      def initialize(method_name)
+        @method_name = method_name
+        source = RubyGolf.method(method_name).to_raw_source(strip_enclosure: true)
+        @size = source.strip.gsub(/\s+/, "").size
+        parser = MethodParser.new(source)
+        @children = RubyGolf.methods(false).map do |method|
+          TestMethod.new(method) if parser.is_method_called?(method)
+        end.compact
+      end
+
+      def cumulative_size
+        @size + @children.map(&:cumulative_size).inject(0, &:+)
+      end
+
+      def self_contained?
+        @children.empty?
+      end
+
+      def called_method_sizes
+        ["#{@method_name}: #{@size}", @children.map(&:called_method_sizes)].flatten.compact
+      end
+    end
 
     def start
       @ergs = []
@@ -19,9 +44,10 @@ module Minitest
       @ergs.sort_by(&:method_name).each do |erg|
         if erg.passed
           begin
-            source = RubyGolf.method(erg.method_name).to_raw_source(strip_enclosure: true)
-            size = source.strip.gsub(/\s+/, "").size
-            io.puts "  #{colorize(erg.method_name, 32)}: #{size} characters"
+            test_method = TestMethod.new(erg.method_name)
+            msg = "  #{colorize(erg.method_name, 32)}: #{test_method.cumulative_size} characters"
+            msg << " (#{test_method.called_method_sizes.join(", ")})" if !test_method.self_contained?
+            io.puts msg
           rescue NoMethodError
             io.puts "  #{colorize(erg.method_name, 31)}: UNDEFINED"
           end
