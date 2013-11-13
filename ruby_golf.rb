01d2073fff4e0881cb8d7424ff68313174501d90
Thomas von Deyen <tvdeyen@gmail.com>
2013-11-13 20:09:50 +0100

    First attempt of solving hole 2.

--- ruby_golf.rb
+++ ruby_golf.rb
@@ -20,7 +20,10 @@ module RubyGolf
   # ouput:  a 'ruby style' version of the identifier: all lowercase, former case
   #         changes to upper case get a prepended underscore
   def self.underscore(s)
-    #s.scan /[A-Z]/,
+    s.sub(/\-/, '').gsub(/([A-Z])/) { |m| s[0] == m ? m.downcase : "_#{m.downcase}" }
+    #res = s.sub(/[A-Z]/, $1 || "_#{$1.downcase}")
+    #puts res
+    #res
   end
 
 
