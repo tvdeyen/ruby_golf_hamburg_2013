262beaffd68e30f28c6bb7298208d76ec9d61410
Thomas Jachmann <self@thomasjachmann.com>
2013-11-13 01:33:55 +0100

    Update metrics gem

--- ruby_golf_test.rb
+++ ruby_golf_test.rb
@@ -4,7 +4,7 @@ require "ruby_golf"
 
 class RubyGolfTest < Minitest::Test
 
-  def test_add
+  def test_hole_1_add
     assert_equal 2, RubyGolf.add(1, 1)
     assert_equal 4, RubyGolf.add(1, 3)
   end
